55d3c9a47bffeb328b80881818c84c6118e72684
lucaschiapella123@gmail.com <Elpepe46507a!>
2025-05-14 11:02:02 -0300

    Inicializacion proyecto

---
+++ proyecto/server.rb
@@ -0,0 +1,19 @@
+require 'sinatra/base'
+
+require 'bundler/setup'
+require 'sinatra/base'
+require 'sinatra/reloader' if Sinatra::Base.environment == :development
+
+class App < Sinatra::Application
+  configure :development do
+    register Sinatra::Reloader
+    after_reload do
+      puts 'Reloaded...'
+    end
+  end
+
+  get '/' do
+    'Seba tonto'
+  end
+end
+
