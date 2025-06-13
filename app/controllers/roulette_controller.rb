948a4733dee439b3b1968fe86655f440e10f1114
Jufaa <juanfrancitorra@gmail.com>
2025-06-13 11:09:19 -0300

    ruletita no anda

---
+++ app/controllers/roulette_controller.rb
@@ -0,0 +1,33 @@
+post '/roulette' do
+  bet = 10
+  user = current_user
+  account = user.account
+
+  if account.balance < bet
+    @error_message = "No tenés saldo suficiente para jugar."
+    return erb :roulette
+  end
+
+  account.balance -= bet
+
+  premio_id = params[:premio_id].to_i
+
+  case premio_id
+  when 0
+    @message = "No ganaste nada."
+  when 1
+    account.balance += 50
+    @message = "¡Premio 1: ganaste 50 monedas!"
+  when 2
+    account.balance += 100
+    @message = "¡Premio 2: ganaste 100 monedas!"
+  when 3
+    @message = "Premio 3: ¡ganaste un bonus especial!"
+  else
+    account.balance += 20
+    @message = "¡Premio #{premio_id}: ganaste 20 monedas!"
+  end
+
+  account.save
+  erb :roulette
+end
