03536a5984fab7018aafc9679ef1039f289c86b5
LucasCh12 <lucaschiapella123@gmail.com>
2025-06-19 23:55:18 -0300

    fix

--- app/spec/controllers/register_controller_spec.rb
+++ app/spec/controllers/register_controller_spec.rb
@@ -12,7 +12,6 @@ RSpec.describe RegisterController do
   it "muestra el formulario de registro" do
     get '/register'
     expect(last_response).to be_ok
-    # Cambié el texto a buscar a algo que seguro está en el template
     expect(last_response.body).to include("<form")
     expect(last_response.body).to include("Correo Electronico")
   end
