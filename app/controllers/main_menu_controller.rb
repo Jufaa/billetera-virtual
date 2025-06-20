1e85ef86e2fae30a98b2e8c45fadca7f0164528e
Jufaa <juanfrancitorra@gmail.com>
2025-06-19 21:01:44 -0300

    historial trans

--- app/controllers/main_menu_controller.rb
+++ app/controllers/main_menu_controller.rb
@@ -4,6 +4,7 @@ class MainMenuController < ApplicationController
   set :views, File.expand_path('../../views', __FILE__)
 
   get '/main_menu' do
+    @all_transfers = all_transfers
     erb :main_menu
   end
 end
